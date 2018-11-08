147da86a450f605a17ff42bdf26c35e38a5c4105
Bartosz Solarski <bartosz.solarski@hotmail.com>
2018-11-08 23:18:05 +0100

    first blood

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## You must add .env file in you service home directory with settings:
 
-  PORT=your_preferred_listening_port_for_server,
-  TOKEN_SECRET: 'your_secret_fot_JWT',
-  ENCODER_SECRET: 'your_secret_for passwd_encoder',
-  MTNS_AND_TNS_FILES_DIRECTORY: 'path_to_directory_with maat.tns and tnsnames.ora files'
+PORT=your_preferred_listening_port_for_server
+TOKEN_SECRET='your_secret_fot_JWT'
+ENCODER_SECRET='your_secret_for passwd_encoder'
+MTNS_AND_TNS_FILES_DIRECTORY='path_to_directory_with maat.tns and tnsnames.ora files'
--- src/authController.js
+++ src/authController.js
@@ -1,9 +1,36 @@
-import forUser from './token';
+import jwt from 'jsonwebtoken';
+import { encoder } from '@ncodefactory/rmui-passwd';
+import { uuid4 } from 'fast-uuid';
 import dbcfg from './dbcfg';
 
+const users = [{ id: 1, login: 'demo', passwd: 'test' }, { id: 2, login: 'maat', passwd: 'hello' }];
 const signin = (req, res) => {
-  const { login, mtns } = req.user;
-  res.json({ token: forUser(req.user), login, mtns });
+  const { login, passwd, mtns } = req.body;
+  if (!login || !passwd || !mtns) {
+    res.status(400).send('Error: Please enter the correct login, passwd and mtns');
+  }
+
+  const user = users.find(u => u.login === login && u.passwd === passwd);
+  if (!user) {
+    res.status(401).send('Error: Invalid login or passwd.');
+  }
+
+  const dbConfig = dbcfg[mtns];
+  if (!dbConfig) {
+    res.status(401).send('Error: Invalid mtns.');
+  }
+
+  const token = jwt.sign(
+    {
+      sub: user.id,
+      login: user.login,
+      mtns,
+      session: uuid4(),
+    },
+    process.env.TOKEN_SECRET,
+    { expiresIn: '3 hours' },
+  );
+  res.status(200).send({ access_token: token });
 };
 
 const fetchMtnses = (req, res) => {
